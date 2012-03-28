29d31311337e1928a4f64d8ab81459674ba06041
Fred Simon <freds@jfrog.com>
2012-03-28 16:56:14 -0600

    Fix Plugs issue

--- jzy3d-api/src/main/java/org/jzy3d/ui/Plugs.java
+++ jzy3d-api/src/main/java/org/jzy3d/ui/Plugs.java
@@ -1,22 +1,28 @@
 package org.jzy3d.ui;
 
 import java.awt.Rectangle;
+import java.lang.reflect.InvocationTargetException;
+
 import org.jzy3d.chart.Chart;
 
 public class Plugs {
 
 	public static void frame(Chart chart){
 		try {
-		Class.forName("");
-		} catch (ClassNotFoundException e) {
+    		Class.forName("org.jzy3d.bridge.PlugsImpl")
+                    .getMethod("frame", Chart.class)
+                    .invoke(null, chart);
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-	}
+    }
 	
 	public static void frame(Chart chart, Rectangle bounds, String title){
 		try {
-		Class.forName("");
-		} catch (ClassNotFoundException e) {
+            Class.forName("org.jzy3d.bridge.PlugsImpl")
+                    .getMethod("frame", Chart.class, Rectangle.class, String.class)
+                    .invoke(null, chart, bounds, title);
+        } catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
